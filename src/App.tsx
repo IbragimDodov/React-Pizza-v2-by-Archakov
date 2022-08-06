5e6abf40656f1ef8b75a42301af339d192f2e312
IbragimDodov <utisey@gmail.com>
2022-08-06 18:58:10 +0300

    27 fin

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
-import Header from './components/Header';
 import Home from './pages/Home';
-import NotFound from './pages/NotFound';
-import Cart from './pages/Cart';
-import FullPizza from './components/FullPizza';
 
 import './scss/app.scss'
 import MainLayout from './layouts/MainLayout';
+
+const Cart = React.lazy(() => import(/* webpackChunkName: "Cart" */ './pages/Cart'))
+const FullPizza = React.lazy(() => import(/* webpackChunkName: "FullPizza" */ './components/FullPizza'))
+const NotFound = React.lazy(() => import(/* webpackChunkName: "NotFound" */ './pages/NotFound'))
 
 function App() {
 
@@ -16,9 +16,9 @@ function App() {
     <Routes>
       <Route path="/" element={<MainLayout/>}>
         <Route path='' element={<Home />} />
-        <Route path='cart' element={<Cart/>} />
-        <Route path='pizza/:id' element={<FullPizza/>} />
-        <Route path='*' element={<NotFound/>} />
+        <Route path='cart' element={<Suspense fallback={<div>loading...</div>}><Cart/></Suspense>} />
+        <Route path='pizza/:id' element={<Suspense fallback={<div>loading...</div>}><FullPizza/></Suspense>} />
+        <Route path='*' element={<Suspense fallback={<div>loading...</div>}><NotFound/></Suspense>} />
       </Route>
     </Routes>
   );
--- src/components/FullPizza.tsx
+++ src/components/FullPizza.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { useParams, useNavigate } from 'react-router-dom'
 
@@ -35,6 +36,11 @@ const FullPizza: React.FC = () => {
       <img src={pizza.imageUrl} alt="" />
       <h2>{pizza.title}</h2>
       <h4> {pizza.price} </h4>
+      <Link to='/'>
+        <button className="button button--outline button--add">
+          <span>back</span>
+        </button>
+      </Link>
     </div>
   )
 }
